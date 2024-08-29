652c8bb5852b62e958b81fc376ea698245bd175d
Eduarda-Gomes <eduardagomesdesouza8@gmail.com>
2024-08-29 14:59:34 -0300

    desenvolvendo banner e menus

--- site-entrelinhas/src/App.jsx
+++ site-entrelinhas/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import logo from "./assets/logo.png";
 import "./App.css";
 import Header from "./components/header/Header";
+import Banner from "./components/banner/Banner";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
--- site-entrelinhas/src/components/banner/Banner.jsx
+++ site-entrelinhas/src/components/banner/Banner.jsx
@@ -1,12 +1,17 @@
-import  "./Banner.css"
-import React from 'react'
+import React from "react";
+import "./Banner.css";
+import banner from "../../assets/banner.png";
 
 const Banner = () => {
   return (
-    <div>
-      
+    <div className="d-flex">
+      <img src={banner} class="img-fluid" alt="" />
+      <div>
+        <h1>REDEV DESENVOLVIMENTOS</h1>
+        <h4>Quem somos e como podemos ajudá-los.</h4>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
