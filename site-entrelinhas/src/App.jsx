a95af27a6fb33777ce720b25b82592aa12e01f54
Rafaele <rafaeleribeiro212@gmail.com>
2024-08-29 11:21:24 -0300

    Atualizações

--- site-entrelinhas/src/App.jsx
+++ site-entrelinhas/src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import logo from "./assets/logo.png";
 import "./App.css";
+import Header from "./components/header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://google.com" target="_blank">
-          <img src={logo} className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>Entrelinhas</h1>
+      <Header />
     </>
   );
 }
---
+++ site-entrelinhas/src/components/header/Header.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../assets/logo.png";
+
+const Header = () => {
+  return (
+    <>
+      <div className="text-center d-flex header">
+        <img src={logo} alt="" />
+        <h1 className="">REDEV DESENVOLVIMENTOS</h1>
+        <ion-icon name="call-outline"></ion-icon>
+        <ion-icon name="mail-outline"></ion-icon>
+      </div>
+      <div className="bg-secondary d-flex justify-content-around">
+        <a className="btn btn-danger btn-lg" href="#">
+          Sobre nós
+        </a>
+        <a className="btn btn-danger btn-lg" href="#">
+          Nossos Serviços
+        </a>
+        <a className="btn btn-danger btn-lg" href="#">
+          Entrelinhas
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Header;
