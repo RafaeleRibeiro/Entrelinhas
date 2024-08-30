0fd86576e9c09bfc20be785454a74a736e1a0570
Eduarda-Gomes <eduardagomesdesouza8@gmail.com>
2024-08-30 09:09:24 -0300

    ajustes finais 1

--- site-entrelinhas/src/components/footer/Footer.jsx
+++ site-entrelinhas/src/components/footer/Footer.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Footer.css";
+import logo3 from "../../assets/logo3.png";
 
-const Footer = () => {
-  return <div className="Container">dedeewsws</div>;
+export const Footer = () => {
+  return (
+    <div className="pt-5">
+      <footer className="footer pt-5">
+        <img src={logo3} alt=""></img>
+        <h4>
+          <strong>
+            Entre em contato com nossa equipe e não perca a oportunidade de
+            fazer seu projeto sair do papel!
+          </strong>
+        </h4>
+        <p></p>
+        <h5>REDevdesenvolvimentos@clientes.com </h5>
+        <p></p>
+        <h5>16-2887-5301</h5>
+       
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
