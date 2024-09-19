275512d593bd2679bcebf46340471865f2efb103
Rafaele <rafaeleribeiro212@gmail.com>
2024-09-19 13:28:18 -0300

    add itens do Footer

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -5,17 +5,19 @@ export const Footer = () => {
   return (
     <div className="pt-5">
       <footer className="footer pt-5">
-        <img src="https://placehold.co/200x200" alt=""></img>
+        {/* <img src="https://placehold.co/200x200" alt=""></img> */}
         <h4>
           <strong>
-            Entre em contato com nossa equipe e não perca a oportunidade de
-            fazer seu projeto sair do papel!
+          Categories
           </strong>
         </h4>
         <p></p>
-        <h5>REDevdesenvolvimentos@clientes.com </h5>
-        <p></p>
-        <h5 className=" pb-5">(16) 2887-5301</h5>
+        <h6>Fiction Books</h6>
+        <h6>Childrens Books</h6>
+        <h6>National Literature</h6>
+        <h6>Non-Fiction Books</h6>
+        <p></p>   
+             
       </footer>
     </div>
   );
