53aafc725cc796bf32fd14717f41dc642ecc2e3a
Rafaele <rafaeleribeiro212@gmail.com>
2024-09-19 14:09:22 -0300

    itens Footer adicionados corretamente

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -7,17 +7,34 @@ export const Footer = () => {
       <footer className="footer pt-5">
         {/* <img src="https://placehold.co/200x200" alt=""></img> */}
         <h4>
-          <strong>
-          Categories
-          </strong>
+          <strong>Categories</strong>
         </h4>
         <p></p>
         <h6>Fiction Books</h6>
         <h6>Childrens Books</h6>
         <h6>National Literature</h6>
         <h6>Non-Fiction Books</h6>
-        <p></p>   
-             
+        <p></p>
+
+        <div>
+          <h5>Agradecemos sua companhia!</h5>
+
+          <a className="btn btn-lg p-3" href="Facebook">
+            <i class="bi bi-facebook"></i>
+          </a>
+
+          <a className="btn btn-lg p-3" href="Twitter">
+            <i class="bi bi-twitter-x"></i>
+          </a>
+
+          <a className="btn btn-lg p-3" href="Youtube">
+            <i class="bi bi-youtube"></i>
+          </a>
+
+          <a className="btn btn-lg p-3" href="Instagram">
+            <i class="bi bi-instagram"></i>
+          </a>
+        </div>
       </footer>
     </div>
   );
