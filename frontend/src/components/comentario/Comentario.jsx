ba5ebfb187c7ec9514cd33a8ff37050302665113
Rafaele <rafaeleribeiro212@gmail.com>
2024-10-11 14:56:15 -0300

    Ajustes comentário, Login

--- frontend/src/components/comentario/Comentario.jsx
+++ frontend/src/components/comentario/Comentario.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import StarRating from "./StarRating"; // Importa o componente de estrelas
+import { Modal, Button } from "react-bootstrap";
 
 const ComentarioModal = ({ googleBooksId }) => {
   const [comentario, setComentario] = useState("");
@@ -9,6 +10,8 @@ const ComentarioModal = ({ googleBooksId }) => {
   const usuarioId = localStorage.getItem("usuarioId"); // Captura o ID do usuário do localStorage
 
   const [showModal, setShowModal] = useState(false);
+
+  const handleCloseModal = () => setShowModal(false);
 
   const handleComentarioChange = (e) => {
     setComentario(e.target.value);
@@ -21,7 +24,7 @@ const ComentarioModal = ({ googleBooksId }) => {
     try {
       const response = await axios.post(apiUrl, {
         usuariosId: usuarioId, // ID do usuário autenticado
-        googleBooksId: "00001", // ID do livro do Google
+        googleBooksId: "00000", // ID do livro do Google
         nota: nota, // Nota do rating por estrelas
         comentario: comentario,
       });
@@ -58,54 +61,98 @@ const ComentarioModal = ({ googleBooksId }) => {
 
       {/* Modal do Bootstrap */}
       {showModal && (
-        <div
-          className="modal show"
-          style={{ display: "block" }}
-          onClick={() => setShowModal(false)}
-        >
-          <div className="modal-dialog color-redev">
-            <div id="comentario" className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">Adicionar Comentário</h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  onClick={() => setShowModal(false)}
-                ></button>
+        <Modal show={showModal} onHide={handleCloseModal} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>Adicionar Comentário</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3 d-flex gap-3 justify-content-center align-items-center">
+                <label>Nota:</label>
+                {/* Componente de estrelas */}
+                <StarRating nota={nota} setNota={setNota} />
               </div>
-              <div className="modal-body">
-                <form onSubmit={handleSubmit}>
-                  <div className="mb-3 d-flex gap-3 justify-content-center align-items-center">
-                    <label>Nota:</label>
-                    {/* Componente de estrelas */}
-                    <StarRating nota={nota} setNota={setNota} />
-                  </div>
-                  <div className="mb-3">
-                    <label>Comentário:</label>
-                    <textarea
-                      className="form-control"
-                      value={comentario}
-                      onChange={handleComentarioChange}
-                      required
-                    />
-                  </div>
-                  <div className="modal-footer d-flex justify-content-center">
-                    <button type="submit" className="btn btn-success">
-                      Enviar Comentário
-                    </button>
-                    {/* <button
+              <div className="mb-3">
+                <label>Comentário:</label>
+                <textarea
+                  className="form-control"
+                  value={comentario}
+                  onChange={handleComentarioChange}
+                  required
+                />
+              </div>
+              <div className="modal-footer d-flex justify-content-center">
+                <button type="submit" className="btn btn-success">
+                  Enviar Comentário
+                </button>
+                {/* <button
                       type="button"
                       className="btn btn-secondary"
                       onClick={() => setShowModal(false)}
                     >
                       Fechar
                     </button> */}
-                  </div>
-                </form>
               </div>
-            </div>
-          </div>
-        </div>
+            </form>
+          </Modal.Body>
+          {/* <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Fechar
+            </Button>
+          </Modal.Footer> */}
+        </Modal>
+
+        // <div
+        //   className="modal show"
+        //   style={{ display: "block" }}
+        //   onClick={() => setShowModal(false)}
+        // >
+        //   <div
+        //     className="modal-dialog color-redev"
+        //     onClick={(e) => e.preventDefault()}
+        //   >
+        //     <div id="comentario" className="modal-content">
+        //       <div className="modal-header">
+        //         <h5 className="modal-title">Adicionar Comentário</h5>
+        //         <button
+        //           type="button"
+        //           className="btn-close"
+        //           onClick={() => setShowModal(false)}
+        //         ></button>
+        //       </div>
+        //       <div className="modal-body">
+        //         <form onSubmit={handleSubmit}>
+        //           <div className="mb-3 d-flex gap-3 justify-content-center align-items-center">
+        //             <label>Nota:</label>
+        //             {/* Componente de estrelas */}
+        //             <StarRating nota={nota} setNota={setNota} />
+        //           </div>
+        //           <div className="mb-3">
+        //             <label>Comentário:</label>
+        //             <textarea
+        //               className="form-control"
+        //               value={comentario}
+        //               onChange={handleComentarioChange}
+        //               required
+        //             />
+        //           </div>
+        //           <div className="modal-footer d-flex justify-content-center">
+        //             <button type="submit" className="btn btn-success">
+        //               Enviar Comentário
+        //             </button>
+        //             {/* <button
+        //               type="button"
+        //               className="btn btn-secondary"
+        //               onClick={() => setShowModal(false)}
+        //             >
+        //               Fechar
+        //             </button> */}
+        //           </div>
+        //         </form>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
       )}
     </div>
   );
