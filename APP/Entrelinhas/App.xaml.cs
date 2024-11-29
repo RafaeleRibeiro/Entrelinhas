677211bdca57887990c1bffee5f658561b20d438
Eduarda-Gomes <eduardagomesdesouza8@gmail.com>
2024-11-29 08:17:03 -0300

    ajustes ProfCastello

---
+++ APP/Entrelinhas/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Entrelinhas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
